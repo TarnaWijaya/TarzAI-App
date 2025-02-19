441dee1be0570969ad4df44877c987da73ba4c70
Tarna <158784522+TarnaWijaya@users.noreply.github.com>
2025-02-19 15:08:56 +0700

    update TarzAI-App/app/src/main/java/com/tarzai/tarnawijaya/Main activity.java

--- TarzAI-App/app/src/main/java/com/tarzai/tarnawijaya/Main activity.java
+++ TarzAI-App/app/src/main/java/com/tarzai/tarnawijaya/Main activity.java
@@ -1,1 +1,20 @@
+package com.tarzai.tarnawijaya;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://tarnawijaya-studio.github.io/Tarz-AI/");
+    }
+}
